fe8302559fa194e26b9c1ff5e2cc3cdc8b4acf6e
Klives Kusuo <kliveskusuo@gmail.com>
2022-06-27 03:49:11 +0100

    oh my god

--- pages/v3/scripts/klivesmovies.js
+++ pages/v3/scripts/klivesmovies.js
@@ -1,4 +1,71 @@
+function ReloadData(){
+    let downloadedMovies = document.getElementById('downloadedmovies');
+    while (downloadedMovies.firstChild) {
+        downloadedMovies.removeChild(downloadedMovies.firstChild);
+    }
+    let ongoingdownloads = document.getElementById('currentdownloads');
+    while (ongoingdownloads.firstChild) {
+        ongoingdownloads.removeChild(ongoingdownloads.firstChild);
+    }
+    LoadMoviesPage();
+}
 
+function LoadMoviesPage() {
+    let downloadedMovies = document.getElementById('downloadedmovies');
+    MakeRequest("/KliveMovie/GetAllDownloadedMovies").then(response => {
+        let json = JSON.parse(response);
+        for (let i = 0; i < json.length; i++) {
+            ConstructMovieIntoGrid('downloadedmovies', JSON.stringify(json[i]));
+        }
+    });
+    MakeRequest("/KliveMovie/GetOngoingMovieDownloads").then(response => {
+        let ongoingdownloads = document.getElementById('currentdownloads');
+        let json1 = JSON.parse(response);
+        for (let i = 0; i < json1.length; i++) {
+            let json = json1[i];
+            let ongoingbox = document.createElement('div');
+            ongoingbox.className="ongoingdownload";
+            let title = document.createElement('span');
+            title.style="font-size: 1.2vw;";
+            title.innerHTML=json.Movie.Title;
+            let date = new Date(json.DownloadStart);
+            let datetime = document.createElement('span');
+            datetime.style="font-size: 1vw; color: var(--secondary);";
+            datetime.innerHTML="Downloading since "+date.toLocaleString();
+            let iscompleted = document.createElement('span');
+            iscompleted.style="font-size: 1vw; color: var(--secondary);";
+            iscompleted.innerHTML="Is Completed: "+json.IsCompleted;
+            ongoingbox.appendChild(title);
+            ongoingbox.appendChild(datetime);
+            ongoingbox.appendChild(iscompleted);
+            ongoingdownloads.appendChild(ongoingbox);
+        }
+    });
+}
+
+function ConstructMovieIntoGrid(gridID, movieJSON) {
+    let moviegrid = document.getElementById(gridID);
+    let json2 = JSON.parse(movieJSON);
+    let movie = document.createElement('div');
+    movie.className = "movieImage";
+    movie.setAttribute("name", JSON.stringify(json2));
+    moviegrid.appendChild(movie);
+    let image = document.createElement("img");
+    if (json2.ImageURL == "") {
+        image.src = "images/noimage.png";
+        image.style = "width: 150px; height: 175px; filter: invert();";
+    }
+    else {
+        image.src = json2.ImageURL;
+        image.style = "width: 150px; height: 175px;";
+    }
+    movie.appendChild(image);
+    let text = document.createElement('span');
+    text.style = "font-weight: bold; align-self: center;";
+    text.innerHTML = json2.Title;
+    movie.appendChild(text);
+    return movie;
+}
 
 function SearchMovie() {
     let value = document.getElementById('moviename').value;
@@ -11,27 +78,9 @@ function SearchMovie() {
         }
         MakeRequest("/KliveMovie/SearchForMovie?moviename=" + value).then(response => {
             let json = JSON.parse(response);
-            for(let i = 0; i<json.length; i++){
-                let json2 = json[i];
-                let movie = document.createElement('div');
-                movie.className="movieImage";
-                movie.setAttribute("name", JSON.stringify(json2));
-                movie.setAttribute("onclick", "DownloadMovie(this.getAttribute('name'))")
-                moviegrid.appendChild(movie);
-                let image = document.createElement("img");
-                if(json2.ImageURL==""){
-                    image.src="images/noimage.png";
-                    image.style="width: 150px; height: 175px; filter: invert();";
-                }
-                else{
-                    image.src=json2.ImageURL;
-                    image.style="width: 150px; height: 175px;";
-                }
-                movie.appendChild(image);
-                let text = document.createElement('span');
-                text.style="font-weight: bold; align-self: center;";
-                text.innerHTML=json2.Title;
-                movie.appendChild(text);
+            for (let i = 0; i < json.length; i++) {
+                let element = ConstructMovieIntoGrid('movies', JSON.stringify(json[i]));
+                element.setAttribute("onclick", "DownloadMovie(this.getAttribute('name'))");
             }
             button.innerHTML = "Search";
         });
@@ -46,7 +95,7 @@ function SearchMovie() {
 
 function DownloadMovie(json) {
     let info = JSON.parse(json);
-    swal("Do you want to download "+info.Title+"?", {
+    swal("Do you want to download " + info.Title + "?", {
         buttons: {
             cancel: "No",
             gopost: {
@@ -56,11 +105,12 @@ function DownloadMovie(json) {
         },
     }).then((value) => {
         let button = document.getElementById('moviebutton');
-        if(value=="download"){
+        button.innerHTML = "Requesting...";
+        if (value == "download") {
             let formdata = new FormData();
             formdata.append("kliveMovieJson", json);
             var xhr = new XMLHttpRequest();
-            xhr.open("POST", api+"/klivemovie/downloadmovie", true);
+            xhr.open("POST", api + "/klivemovie/downloadmovie", true);
             xhr.send(formdata);
             xhr.onreadystatechange = function () {
                 if (xhr.readyState == 4 && xhr.status == 200) {
@@ -68,14 +118,20 @@ function DownloadMovie(json) {
                     setTimeout(function () {
                         button.innerHTML = "Search";
                     }, 2000)
+                    setTimeout(function () {
+                        ReloadData();
+                    }, 500)
                 }
-                else{
+                else {
                     button.innerHTML = "Failed.";
                     setTimeout(function () {
                         button.innerHTML = "Search";
                     }, 2000)
                 }
             }
+        }
+        else{
+            button.innerHTML = "Search";
         }
     })
 }
